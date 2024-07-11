f3f29e517ddec6a932e935263b05b4a1317524e5
Manya7547 <52461524+Manya7547@users.noreply.github.com>
2024-07-10 20:36:23 -0700

    200 corrected

--- 200_Number_of_Islands.java
+++ 200_Number_of_Islands.java
@@ -1,42 +1,37 @@
-static int[] x_dir = { 0, 0, 0, 1, 1, 1, -1, -1, -1 };
-static int[] y_dir = { 1, 0, -1, 1, 0, -1, 1, 0, -1 };
+class Solution {
+    public int numIslands(char[][] grid) {
+        int[] x_dir = { 0, 0, 1, -1 };
+        int[] y_dir = { -1, 1, 0, 0 };
+        int count = 0;
 
-static Integer count_islands(ArrayList<ArrayList<Integer>> matrix) {
-    int count = 0;
-    for (int i = 0; i < matrix.size(); i++) {
-        for (int j = 0; j < matrix.get(0).size(); j++) {
-            if (matrix.get(i).get(j) == 1) {
-                count++;
-                helper(matrix, matrix.get(i).get(j), i, j);
+        for (int i = 0; i < grid.length; i++) {
+            for (int j = 0; j < grid[0].length; j++) {
+                if (grid[i][j] == '1') {
+                    count++;
+                    bfs(grid, i, j, x_dir, y_dir);
+                }
             }
         }
+        return count;
     }
-    return count;
-}
 
-static void helper(ArrayList<ArrayList<Integer>> matrix, int s, int i, int j) {
-    // bfs search - store the position of source in a queue
-    Queue<int[]> q = new LinkedList<>();
-    q.add(new int[] { i, j });
-    matrix.get(i).set(j, 0);
-
-    while (!q.isEmpty()) {
-        int[] head = q.poll();
-        int curr_row = head[0];
-        int curr_col = head[1];
-
-        // look in 8 directions
-        for (int k = 0; k < 8; k++) {
-            int new_row = curr_row + x_dir[k];
-            int new_col = curr_col + y_dir[k];
-            // if out of bound
-            if (new_row < 0 || new_row >= matrix.size() || new_col < 0 || new_col >= matrix.get(0).size())
-                continue;
-
-            if (matrix.get(new_row).get(new_col) == 1) {
-                q.add(new int[] { new_row, new_col });
-                // visited 1's will become 0
-                matrix.get(new_row).set(new_col, 0);
+    private void bfs(char[][] grid, int i, int j, int[] x_dir, int[] y_dir) {
+        Queue<int[]> q = new LinkedList<>();
+        q.add(new int[] { i, j });
+        grid[i][j] = '0';
+        while (!q.isEmpty()) {
+            int[] head = q.poll();
+            int curr_row = head[0];
+            int curr_col = head[1];
+            for (int k = 0; k < 4; k++) {
+                int new_row = curr_row + x_dir[k];
+                int new_col = curr_col + y_dir[k];
+                if (new_row < 0 || new_col < 0 || new_row >= grid.length || new_col >= grid[0].length)
+                    continue;
+                if (grid[new_row][new_col] == '1') {
+                    q.add(new int[] { new_row, new_col });
+                    grid[new_row][new_col] = '0';
+                }
             }
         }
     }
